5dbccb53c6ba134af791efa685675c8fbdde7eaf
Guoyu Chen <gdchenguoyu@gmail.com>
2021-09-24 11:34:29 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // 请求地址
+    hostUrl: "http://raasr.xfyun.cn/api/",
+    // 在控制台-我的应用-语音转写获取
+    appId: "29b5726a",
+    // 在控制台-我的应用-语音转写获取
+    secretKey: "38e0c4a96d54f3a2e29a891dcc36a513"
+}
